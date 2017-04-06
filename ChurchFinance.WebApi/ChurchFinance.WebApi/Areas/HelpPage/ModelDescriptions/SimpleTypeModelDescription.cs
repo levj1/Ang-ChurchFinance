be8ba93725ca1620ad87fe118953769f8105a5a7
levj1 <levj1@yahoo.com>
2017-04-06 10:05:13 -0400

    Create church finance project angular version

---
+++ ChurchFinance.WebApi/ChurchFinance.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ChurchFinance.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ChurchFinance.WebApi/ChurchFinance.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChurchFinance.WebApi.WebApiApplication" Language="C#" %>
